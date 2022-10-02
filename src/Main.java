e8ca420a55b01480a9ec25d827fe7efc801fa2b5
Юля <potemkin.vg@yandex.ru>
2022-10-02 21:22:49 +0300

    Задание готово.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        var TotalTime = 640;
+        var NumberOfPersons = TotalTime / 8;
+        System.out.println("Всего работников в компании - " + NumberOfPersons + " человек");
+
+        var NewNumberOfPerson = NumberOfPersons + 94;
+        var NewTotalTime = NewNumberOfPerson * 8;
+        System.out.println("Если в компании работает " + NewNumberOfPerson + " человек, то всего " + NewTotalTime + " часов работы может быть поделено между сотрудниками.");
+
     }
 }
